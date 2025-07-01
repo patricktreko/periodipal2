e1239d1d54e5ff26da3bab997e45eee61850df89
patrick <ocanapatrick10@gmail.com>
2025-07-01 20:30:07 +0800

    Update app.py

--- periodipalvs/app.py
+++ periodipalvs/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, abort
 import json
+import os
 
 app = Flask(__name__)
 
@@ -49,5 +50,6 @@ def minigame_quiz():
 def minigame_sorting():
     return render_template('minigame_sorting.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
